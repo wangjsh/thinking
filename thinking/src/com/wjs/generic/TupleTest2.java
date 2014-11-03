a7af701482139ec14959c60c3a544d3b26ebedf8
wangjsh <wangjsh@yahoo.com>
2014-11-03 09:33:37 +0800

    第六次

---
+++ thinking/src/com/wjs/generic/TupleTest2.java
@@ -0,0 +1,25 @@
+package com.wjs.generic;
+
+public class TupleTest2 {
+	
+	static TwoTuple<String,Integer> f()
+	{
+		return Tuple.tuple("hi",47);
+	}
+	static TwoTuple f2()
+	{
+		return Tuple.tuple("hi", 47);
+	}
+	static ThreeTuple<String,Integer,Double> f3()
+	{
+		return Tuple.tuple("hello", 12, 1.234);
+	}
+	public static void main(String[] args)
+	{
+		TwoTuple<String,Integer> ttsi=f();
+		System.out.println(ttsi);
+		System.out.println(f2());
+		System.out.println(f3());
+	}
+
+}
